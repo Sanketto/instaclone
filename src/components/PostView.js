650b942ec183be895fc31a3d0103bec41b6692e5
Sanket Ghodnadikar <124677097+Sanketto@users.noreply.github.com>
2023-03-02 12:42:37 +0530

    atlas url updated

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -5,11 +5,46 @@ import more from '../images/more_icon.png'
 import heart from '../images/heart.png'
 import Header from "./Header";
 import axios from "axios";
+const user = [{
+    name: "Sanket",
+  location: "Pune",
+  description: "Jungle",
+  date: "2023/2/24",
+  likes: 201,
+  postImage: "https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg"
+
+},
+{
+    name: "Ai",
+  location: "Tokyo",
+  description: "Mt. Fuji Japan",
+  date: "2023/2/24",
+  likes: 5000,
+  postImage: "https://www.japanrailpass.com.au/wp-content/uploads/2014/07/Aerial-view-of-Mount-Fuji-volcano-with-a-snow-cap-in-Japan.jpg"
+
+},
+{
+    name: "John",
+  location: "Mumbai",
+  description: "Gateway of India",
+  date: "2023/2/24",
+  likes: 1115,
+  postImage: "https://planetofhotels.com/guide/sites/default/files/styles/paragraph__hero_banner__hb_image__1880bp/public/hero_banner/Gateway-to-India_0.jpg"
+},
+{
+    name: "Doe",
+  location: "Banglore",
+  description: "Maisoor Palace",
+  date: "2023/2/24",
+  likes: 4000,
+  postImage: "https://karnatakatourism.org/wp-content/uploads/2020/06/mysore-palace-1.jpg"
+}
+]
 
 export default function PostView() {
    const[data, setLike] = useState([])
    useEffect(()=>{
-     axios.get('http://localhost:3000/postview')
+     axios.get('https://instbackend.onrender.com/postview')
      .then(res=>{
         setLike(res.data)
      })
@@ -24,7 +59,7 @@ export default function PostView() {
                     break;
                 }
             } 
-            axios.put(`http://localhost:3000/create/${data._id}`,data)
+            axios.put(`https://instbackend.onrender.com/create/${data._id}`,data)
             return [...prevData]
          })
     }
@@ -32,7 +67,7 @@ export default function PostView() {
     return <>
         <Header />
         {
-            data.map(data => {
+            user.map(data => {
                 return <>
                     <div className="post-container">
                         <header>
